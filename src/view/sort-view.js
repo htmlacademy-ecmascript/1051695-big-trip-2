00da73719eb9da777ff0342a05d69451eeb5bcc1
DmitryOO <dimkut@mail.ru>
2026-01-07 17:38:09 +0400

    5.2.1

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -22,7 +22,7 @@ function createSortTemplate() {
 
     <div class="trip-sort__item  trip-sort__item--price">
       <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price"
-      data-sort-type="${SortType.TIME}">
+      data-sort-type="${SortType.PRICE}">
       <label class="trip-sort__btn" for="sort-price">Price</label>
     </div>
 
